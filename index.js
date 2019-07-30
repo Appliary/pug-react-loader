9d4ef0a8a1605468d7f37340221a839348c3d1ad
Michaël Carrette <miccarr@me.com>
2019-07-30 12:09:03 +0200

    Handle empty pug files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const PRC = require('./compile');
 
 module.exports = function PugTemplate(source) {
+  if(!source) throw `File "${this.resource}" is empty`
   let out = 'const React = require(\'react\');\n';
   out += PRC.compileClient(source, {
     filename: this.resource,
