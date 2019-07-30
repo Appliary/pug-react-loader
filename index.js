8042058eb8d5d8870db2594b0d59b97be2a8e3b0
Michaël Carrette <miccarr@me.com>
2019-07-30 12:36:21 +0200

    Handle empty pug files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function PugTemplate(source) {
   let out = 'const React = require(\'react\');\n';
   out += PRC.compileClient(source, {
     filename: this.resource,
-    class: this.rawRequest,
+    component: this.resource.slice(this.rootContext.length),
   });
   return out;
 };
